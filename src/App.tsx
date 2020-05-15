a372f1a100af5ad9717743793a5b0fa438dcb5bf
“Yenn <yennlou21@gmail.com>
2020-05-15 13:24:35 +1200

    add ui: wave select & octave control

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { fromEvent } from 'rxjs'
 import { pluck, filter } from 'rxjs/operators'
 import { playTone, stopTone, getKeyList, key2freq } from './piano'
 
-import './styles.scss'
+import './styles/main.scss'
 
 const App = () => {
   useEffect(() => {
@@ -22,7 +22,24 @@ const App = () => {
   }, [])
   return (
     <div id="root">
-      <div id="app">Hello Preact</div>
+      <div id="app">
+        <div className="config">
+          <ul>
+            <li class="config__select-wave">
+              <select name="control">
+                <option value="sine">sine</option>
+                <option value="square">square</option>
+                <option value="sawtooth">sawtooth</option>
+                <option value="triangle">triangle</option>
+              </select>
+            </li>
+            <li class="config__control-octave">
+              <button>+</button>
+              <button>-</button>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
