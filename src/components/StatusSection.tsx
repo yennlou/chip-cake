5b639ea83acb4498f3a4b46ab87ab37a6d70198e
“Yenn <yennlou21@gmail.com>
2020-05-23 16:44:17 +1200

    paint some weird wave

--- src/components/StatusSection.tsx
+++ src/components/StatusSection.tsx
@@ -1,5 +1,5 @@
 import { h } from 'preact'
-import { useEffect } from 'preact/compat'
+import { useEffect, useRef } from 'preact/compat'
 import { interval } from 'rxjs'
 import { Analyser } from '../audio'
 
@@ -7,11 +7,27 @@ interface StatusSectionProps {
   analyser: Analyser
 }
 
+const updateWave = (analyser: Analyser, canvas: HTMLCanvasElement) => {
+  const amplitudeArray = analyser.getTimeDomainData()
+  const ctx = canvas.getContext('2d')!
+  const width = canvas.offsetWidth
+  const height = canvas.offsetHeight
+  ctx.clearRect(0, 0, width, height)
+  ctx.fillStyle = '#000000'
+  amplitudeArray.forEach((amplitude, idx) => {
+    const x = (idx * width) / 1024
+    const y = height - (amplitude * height) / 256
+    // console.log(y)
+    ctx.fillRect(x, y, 1, 1)
+  })
+}
+
 const StatusSection = ({ analyser }: StatusSectionProps) => {
+  const canvasEl = useRef(null)
   useEffect(() => {
-    const timer$ = interval(1000)
+    const timer$ = interval(100)
     const sub = timer$.subscribe(() => {
-      // console.log(analyser.getTimeDomainData())
+      updateWave(analyser, canvasEl.current!)
     })
     return () => {
       sub.unsubscribe()
@@ -20,7 +36,7 @@ const StatusSection = ({ analyser }: StatusSectionProps) => {
   return (
     <div className="status">
       <div className="status__wave">
-        <canvas></canvas>
+        <canvas ref={canvasEl}></canvas>
       </div>
     </div>
   )
